0b247afb8339ab8042dd0d7c3f853d880024b352
adiko <dikodam@googlemail.com>
2019-03-26 08:54:14 +0100

    code formatted

--- src/main/java/ohm/softa/a01/MyExampleClass.java
+++ src/main/java/ohm/softa/a01/MyExampleClass.java
@@ -1,11 +1,12 @@
 package ohm.softa.a01;
 
 public class MyExampleClass {
-	/**
-	 * Basic integer multiplication
-	 * @return product of a and b
-	 */
-	public int multiply(int a, int b) {
-		return a*b;
-	}
+    /**
+     * Basic integer multiplication
+     *
+     * @return product of a and b
+     */
+    public int multiply(int a, int b) {
+        return a * b;
+    }
 }
--- src/test/java/ohm/softa/a01/MyExampleClassTest.java
+++ src/test/java/ohm/softa/a01/MyExampleClassTest.java
@@ -10,13 +10,13 @@ class MyExampleClassTest {
     @Test
     void testMultiply() {
         IntStream.range(-5, 5)
-                .forEach(i -> assertEquals(i * (i - 1), new MyExampleClass().multiply(i, i - 1)));
+                 .forEach(i -> assertEquals(i * (i - 1), new MyExampleClass().multiply(i, i - 1)));
     }
-
+    
     @Test
     void testMultiply2() {
         MyExampleClass myExampleClass = new MyExampleClass();
         IntStream.range(-5, 5)
-                .forEach(i -> assertEquals(i * (i-1), myExampleClass.multiply(i, i-1)));
+                 .forEach(i -> assertEquals(i * (i - 1), myExampleClass.multiply(i, i - 1)));
     }
 }
